e5e7a0cc24535f95774d5232e2e931a287b76e54
Shirley Laurette <Shirleylaurette1@gmail.com>
2023-10-30 15:23:51 +0100

    coworking landing app landing page creation

---
+++ coworking-app-landing-page/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className="bg-primary w-full text-5xl overflow-hidden">
+      <h1>Hello World</h1>
+    </div>
+  )
+}
+
+export default App
